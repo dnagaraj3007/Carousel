9e26f19c5f9cc21a245b021045f8a240ceba70bc
nadanda <nadanda@NADANDA-IN.in.oracle.com>
2021-01-09 22:41:35 +0530

    adding item name, prices, category

--- src/carousel.js
+++ src/carousel.js
@@ -1,4 +1,5 @@
-let counter = 1;
+let counter = 0;
+const size =
 
 document.addEventListener("DOMContentLoaded",function(){
     addProducts(products);
@@ -8,49 +9,86 @@ document.addEventListener("DOMContentLoaded",function(){
 function addProducts(products) {
     products.forEach((product) => {
         createCellAndAddImg(product);
-        createCellIndicator();
+        //createCellIndicator();
     })
 }
 
 function createCellAndAddImg(product){
     let carouselList = document.getElementById("container");
+    let card = document.createElement("div");
+    card.className="carousel_card";
+
     let image = document.createElement("img");
     image.src = product.src;
     image.className= "carousel_image";
 
-    carouselList.appendChild(image);
+    let details = document.createElement("div");
+    details.className="carousel_card_container"
+
+    let itemName = document.createElement("h4");
+    itemName.className="carousel_card_item_name";
+    itemName.textContent=product.name;
+
+    let breakLine = document.createElement("br");
+
+    let itemPrice = document.createElement("h5");
+    itemPrice.className = "carousel_card_item_price";
+    itemPrice.textContent = "Price: "+product.price
+
+    let itemCategory = document.createElement("h5");
+    itemCategory.className = "carousel_card_item_category";
+    itemCategory.textContent = "Category: "+product.category;
+
+    details.appendChild(itemName);
+    details.appendChild(breakLine);
+    details.appendChild(itemPrice);
+    details.appendChild(breakLine);
+    details.appendChild(itemCategory);
+
+    card.appendChild(image);
+    card.appendChild(details);
+    carouselList.appendChild(card);
 }
 
-function createCellIndicator(){
+/*function createCellIndicator(){
     let carouselNav = document.getElementById("carousel_nav");
     let button = document.createElement("button");
     button.className="carousel_indicator";
     carouselNav.appendChild(button);
-}
+}*/
 
 function addButtonListeners(){
     const carouselContainer  = document.querySelector(".carousel_container")
-    const image = document.querySelector(".carousel_container img")
+    const image = document.querySelector(".carousel_card")
     const prevBtn = document.querySelector("#prevBtn");
     const nextBtn = document.querySelector("#nextBtn");
-    const size = image.clientWidth;
+    let size;
     nextBtn.addEventListener('click', ()=>{
+       size = image.clientWidth;
         carouselContainer.style.transition = "transform 0.4s ease-in-out"
         counter++;
         carouselContainer.style.transform = 'translateX('+(-size*counter)+'px)'
         console.log(carouselContainer.style.transform);
     })
     prevBtn.addEventListener('click', ()=>{
-        carouselContainer.style.transition = "transform 0.4s ease-in-out"
-        counter--;
-        carouselContainer.style.transform = 'translateX('+(-size*counter)+'px)'
-        console.log(carouselContainer.style.transform);
-    })
-    carouselContainer.addEventListener('transitioned', ()=>{
-        console.log("Triggered");
-    })
+        size = image.clientWidth;
+        if(counter ===0){
+            counter= products.length-2;
+        }
 
-
+            carouselContainer.style.transition = "transform 0.4s ease-in-out"
+            counter--;
+            carouselContainer.style.transform = 'translateX(' + (-size * counter) + 'px)'
+            console.log(carouselContainer.style.transform);
+    })
+    carouselContainer.addEventListener('transitionend', ()=>{
+        if(products.length>3) {
+            if (counter === products.length - 2) {
+                counter = 0;
+                carouselContainer.style.transform = 'translateX(' + (-size * counter) + 'px)'
+            }
+        }
+    })
 }
 
 
--- src/products.js
+++ src/products.js
@@ -1,32 +1,56 @@
 var products= [{
-  "name": "A",
+  "name": "Donuts",
   "src": "images/1.jpg",
-  "price": "10000"
+  "price": "100",
+  "category": "Desserts"
 },
   {
-    "name": "B",
+    "name": "Laddoo",
     "src": "images/2.jpg",
-    "price": "20000"
+    "price": "200",
+    "category": "Desserts"
   },
   {
-    "name": "C",
+    "name": "Pastry",
     "src": "images/3.jpg",
-    "price": "30000"
+    "price": "100",
+    "category": "Desserts"
   },
   {
-    "name": "D",
+    "name": "Fried Rice",
     "src": "images/4.jpg",
-    "price": "30000"
+    "price": "300",
+    "category": "Main course"
   },
   {
-    "name": "E",
+    "name": "Roti",
     "src": "images/5.jpg",
-    "price": "30000"
+    "price": "180",
+    "category": "Main course"
   },
   {
-    "name": "F",
+    "name": "Veg Thali",
     "src": "images/6.jpg",
-    "price": "30000"
-  }
+    "price": "300",
+    "category": "Main course"
+  },
+  {
+    "name": "Bubble Tea",
+    "src": "images/7.jpg",
+    "price": "80",
+    "category": "Beverages"
+  },
+  {
+    "name": "Rasna",
+    "src": "images/8.jpg",
+    "price": "50",
+    "category": "Beverages"
+  },
+  {
+    "name": "Squash",
+    "src": "images/9.jpg",
+    "price": "120",
+    "category": "Beverages"
+  },
 
 ]
