0e6c36aaa69a2675e111d1de51224c69be134eef
nadanda <nadanda@NADANDA-IN.in.oracle.com>
2021-01-09 23:34:01 +0530

    adding categories

--- src/carousel.js
+++ src/carousel.js
@@ -1,19 +1,59 @@
 let counter = 0;
-const size =
+let maxProducts = products.length;
+
 
 document.addEventListener("DOMContentLoaded",function(){
-    addProducts(products);
+    addProducts(products,"");
     addButtonListeners();
+    addItemCategories(products);
+    addCategoryListener();
 });
 
-function addProducts(products) {
+function addItemCategories(products){
+    const categoryField = document.querySelector("#categories");
+    const categories =  [...new Set(products.map(product => product.category))];
+    const option = document.createElement('option');
+    option.text = "";
+    option.value= "";
+    categoryField.appendChild(option);
+    categories.forEach((category)=>{
+        const option = document.createElement('option');
+        option.text = category;
+        option.value= category;
+        categoryField.appendChild(option);
+    })
+}
+
+function addCategoryListener(){
+    const categoryField = document.querySelector("#categories");
+    categoryField.addEventListener('change', function(){
+        changeDisplayType(products, categoryField.value);
+    })
+}
+
+function changeDisplayType(products, filter){
+    maxProducts=0;
+    counter = 0;
+    const cards = document.querySelector(".carousel_container").children;
+    for(let i=0;i<products.length; i++){
+        if(products[i].category===filter){
+            cards[i].style.display= "inline"
+            maxProducts++;
+        }else{
+            cards[i].style.display= "none";
+        }
+    }
+
+}
+
+function addProducts(products, filter) {
     products.forEach((product) => {
-        createCellAndAddImg(product);
+            createCellAndAddImg(product);
         //createCellIndicator();
     })
 }
 
-function createCellAndAddImg(product){
+function createCellAndAddImg(product, displayType){
     let carouselList = document.getElementById("container");
     let card = document.createElement("div");
     card.className="carousel_card";
@@ -73,7 +113,7 @@ function addButtonListeners(){
     prevBtn.addEventListener('click', ()=>{
         size = image.clientWidth;
         if(counter ===0){
-            counter= products.length-2;
+            counter= maxProducts-2;
         }
 
             carouselContainer.style.transition = "transform 0.4s ease-in-out"
@@ -82,12 +122,11 @@ function addButtonListeners(){
             console.log(carouselContainer.style.transform);
     })
     carouselContainer.addEventListener('transitionend', ()=>{
-        if(products.length>3) {
-            if (counter === products.length - 2) {
+            if (counter === maxProducts - 2) {
                 counter = 0;
                 carouselContainer.style.transform = 'translateX(' + (-size * counter) + 'px)'
             }
-        }
+
     })
 }
 
